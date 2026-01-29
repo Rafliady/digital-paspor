fe4ad06ef786242409ad7c4bf1d8c525623ffeb0
aachrifai <rifaiachmad003@gmail.com>
2026-01-29 09:33:27 +0700

    finally2

--- resources/views/form-paspor.blade.php
+++ resources/views/form-paspor.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Layanan Paspor Digital</title>
+    
+    <link rel="icon" type="image/png" href="{{ asset('img/logo_imigrasi.png') }}">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     
