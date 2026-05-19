19b35f1c037db14a1e98475a55a9b092a5905223
Rafliady <rafliadi543@gmail.com>
2026-05-19 08:12:33 +0700

    first commit

--- resources/views/form-paspor.blade.php
+++ resources/views/form-paspor.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Layanan Paspor Digital</title>
+    <title>SIPANDA-JIWO</title>
     <link rel="icon" type="image/png" href="{{ asset('img/logo_imigrasi.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -41,7 +41,8 @@
 <div class="container mt-4">
     <div id="header-utama" class="text-center mb-5 fade-in">
         <img src="{{ asset('img/logo_imigrasi.png') }}" class="header-logo" alt="Logo">
-        <h3 class="fw-bold text-dark">LAYANAN PASPOR DIGITAL</h3>
+        <h3 class="fw-bold text-dark">SIPANDA</h3>
+        <h3 class="fw-bold text-dark">SISTEM PENGISIAN DATA PASPOR</h3>
         <p class="text-muted">Kantor Imigrasi Kelas II Non TPI Wonosobo</p>
     </div>
 
